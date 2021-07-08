68e7898d83694aad8673138f94cfd33cfa03b5d4
Alec <34276618+alecgrthomas@users.noreply.github.com>
2021-07-08 13:47:16 -0400

    Delete rdtx_2021 directory

--- rdtx_2021/auxiliary/rdtx_smooth.m
+++
@@ -1,7 +0,0 @@
-% This function performs a <numpoint> point smooth of a function f
-% returns fsmooth
-% function[fsmooth]= rdtx_smooth(f,numpoints) 
-function[fsmooth]= rdtx_smooth(f,numpoints) % Size of the averaging window
-
-window = ones(numpoints,numpoints)/numpoints/numpoints; 
-fsmooth = convn(f,window,'same');
